500e3c2475add9815905022474a5ad688f11db98
Isaac Sheptovitsky <isaacsheptovitsky@Isaacs-MBP.Dlink>
2019-10-19 10:29:34 +0300

    added color functionality

--- src/script.js
+++ src/script.js
@@ -15,7 +15,6 @@ export default function() {
 
   let createdStyles = 0;
 
-
   // model
   let model = {
     "alignment": {
@@ -31,6 +30,40 @@ export default function() {
     }
   };
 
+  function generateColors(shapeLayer) {
+    model.colors[shapeLayer.name] = shapeLayer.style.fills[0].color;
+  }
+
+  function generateTextStyles(textLayer) {
+    const alignmentEntries = Object.entries(model.alignment);
+    const colorEntries = Object.entries(model.colors);
+    let originalStyle = textLayer.style;
+
+    // loop through alignments and go only for those that are true
+    for (var i = 0; i < alignmentEntries.length; i++) {
+
+      // check if alignment value is true
+      if (alignmentEntries[i][1]) {
+
+        // loop through colors and create styles
+        for (var j = 0; j < colorEntries.length; j++) {
+          textLayer.style.alignment = alignmentEntries[i][0];
+          textLayer.style.textColor = colorEntries[j][1];
+
+          // push the style
+          document.sharedTextStyles.push({
+            name: `${textLayer.name} / ${alignmentEntries[i][0]} / ${colorEntries[j][0]}`,
+            style: textLayer.style
+          });
+          createdStyles++;
+        }
+        // end of color loop
+      }
+    }
+    // end of alignment loop
+    textLayer.style.alignment = alignmentEntries[0][0];
+    textLayer.style.textColor = colorEntries[0][1];
+  }
 
   // validate selected layers have at least one text layer
   if (selectedLayers.length <= 0 && selectedTextLayers.length == 0) {
@@ -38,46 +71,24 @@ export default function() {
   } else if (selectedLayers.length > 0 && selectedTextLayers.length == 0) {
     UI.message(`Select some text layers as well plz...`)
   } else {
-
-    selectedTextLayers.forEach();
-
-
-    const alignmentEntries = Object.entries(model.alignment);
-    const colorEntries = Object.entries(model.colors);
-
-    // loop through the selected layers
-    for (let textLayer of selectedLayers) {
-
-      let originalStyle = textLayer.style;
-
-      // loop through alignments and go only for those that are true
-      for (var i = 0; i < alignmentEntries.length; i++) {
-        // check if alignment value is true
-        if (alignmentEntries[i][1]) {
-
-          // loop through colors and create styles
-          for (var j = 0; j < colorEntries.length; j++) {
-            textLayer.style.alignment = alignmentEntries[i][0];
-            textLayer.style.textColor = colorEntries[j][1];
-            // push the style
-            document.sharedTextStyles.push({
-              name: `${textLayer.name} / ${alignmentEntries[i][0]} / ${colorEntries[j][0]}`,
-              style: textLayer.style
-            });
-            createdStyles++;
-          }
-          // end of color loop
-        }
-      }
-      // end of alignment loop
-      textLayer.style.alignment = alignmentEntries[0][0];
-      textLayer.style.textColor = colorEntries[0][1];
+    // UI.message(`⏳This might take a few seconds`)
+    // loop through the selected shape layers and update colors
+    if (selectedShapeLayers.length > 0) {
+      // reset model.colors
+      model.colors = {};
+      // populate model.colors from selectedShapeLayers
+      selectedShapeLayers.forEach(generateColors);
     }
+
+    // loop through the selected text layers and create styles
+    selectedTextLayers.forEach(generateTextStyles);
   }
+
+  // message at the end
   if (createdStyles > 0) {
-    UI.message(`🤟 Rock On 🤟 ${createdStyles} styles were created!`)
+    UI.message(`🤟 Rock On 🤟 ${createdStyles} styles were created!!!`)
   } else {
     UI.message(`🤦🏼‍♀️ Nothing happend...`)
   }
 
-};
+}
