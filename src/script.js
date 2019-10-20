4ab8f7561b21c0a8b8d2401e5f02c1e5ad1532f7
Vitsky <vitskyds@gmail.com>
2019-10-20 10:35:38 +0300

    started work on UI

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
+import BrowserWindow from 'sketch-module-web-view'
+import { getWebview } from 'sketch-module-web-view/remote'
+
 import sketch from 'sketch'
+import UI from 'sketch/ui'
+
+const webviewIdentifier = 'webview.webview'
 
 export default function() {
 
-  const sketch = require('sketch')
-  const UI = require('sketch/ui')
-
   const Document = require('sketch/dom').Document;
-
 
   const document = Document.getSelectedDocument();
   const page = document.selectedPage;
@@ -88,9 +90,42 @@ export default function() {
 
   // message at the end
   if (createdStyles > 0) {
-    UI.message(`🤟 Rock On 🤟 ${createdStyles} styles were created!!!`)
+    UI.message(`🤟 Rock On 🤟 ${createdStyles} styles were created!`)
   } else {
     UI.message(`🤦🏼‍♀️ Nothing happend...`)
   }
+
+  // webview
+  const options = {
+    identifier: webviewIdentifier,
+    width: 240,
+    height: 180,
+    show: false
+  }
+
+  const browserWindow = new BrowserWindow(options)
+
+  // only show the window when the page has loaded to avoid a white flash
+  browserWindow.once('ready-to-show', () => {
+    browserWindow.show()
+  })
+
+  const webContents = browserWindow.webContents
+
+  // print a message when the page loads
+  webContents.on('did-finish-load', () => {
+    UI.message('UI loaded!')
+  })
+
+  // add a handler for a call from web content's javascript
+  webContents.on('nativeLog', s => {
+    UI.message(s)
+    webContents
+      .executeJavaScript(`setRandomNumber(${Math.random()})`)
+      .catch(console.error)
+  })
+
+  browserWindow.loadURL(require('../resources/webview.html'))
+
 
 }
