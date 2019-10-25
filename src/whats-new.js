a651d27acbb4d2afef979cb18bf6c899828fd07e
Vitsky <vitskyds@gmail.com>
2019-10-25 13:51:46 +0300

    started anew

--- src/whats-new.js
+++
@@ -1,7 +0,0 @@
-import sketch from 'sketch'
-
-export default function() {
-  var UI = require('sketch/ui')
-  const Document = require('sketch/dom').Document;
-  NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString("https://github.com/VitskyDs/tin-drum"));
-}
